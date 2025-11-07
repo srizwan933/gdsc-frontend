48ad9851fd40d373fa420817ebac55946bcc3866
rkkapdnis <rkkapdnis1996@gmail.com>
2025-11-07 13:33:59 +0530

    create the authservice and some change in forget-password ts file . Api calling of forget-password

---
+++ src/app/core/service/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private baseUrl='http://localhost:5000/api/auth';
+
+  constructor(private http:HttpClient) { }
+  forgotPassword(email:string):Observable<any>{
+    return this.http.post(`${this.baseUrl}/forget-password`,
+      {email});
+  }
+   resetPassword(token:string,password:string):Observable<any>{
+    return this.http.post(`${this.baseUrl}/reset-password/{$token}`,{password});
+   }
+}
